273e1ec6553e7eaef98d45ba957e190e3a089865
CLL-CMU <134076423+CLL-CMU@users.noreply.github.com>
2024-10-23 14:13:35 +0800

    Update Load_save_models.py
    Load_save_models

--- Main/Model/Load_save_models.py
+++ Main/Model/Load_save_models.py
@@ -1,7 +1,7 @@
 import torch
 
 def save_model(model, optimizer, epoch, val_loss, other_params, save_path):
-    # 保存模型、优化器和其他参数的状态
+    # Save the state of the model, optimizer, and other parameters
     torch.save({
         'epoch': epoch,
         'model_state_dict': model.state_dict(),
@@ -10,13 +10,12 @@ def save_model(model, optimizer, epoch, val_loss, other_params, save_path):
         'other_params': other_params,
     }, save_path)
 
-
 def load_checkpoint(model, optimizer, checkpoint_path):
     checkpoint = torch.load(checkpoint_path)
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     epoch = checkpoint['epoch']
     val_loss = checkpoint['val_loss']
-    other_params = checkpoint.get('other_params', {})  # 获取其他参数，如果不存在则默认为空字典
-    
+    other_params = checkpoint.get('other_params', {})  # Retrieve other parameters, default to an empty dictionary if not found
+
     return model, optimizer, epoch, val_loss, other_params
